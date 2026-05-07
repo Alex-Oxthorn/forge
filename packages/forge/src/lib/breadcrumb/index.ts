782b527b402ef6af9852bc89da3f2137583c447a
Alex Oxthorn <alex.oxthorn@tylertech.com>
2026-05-07 09:25:31 -0400

    chore: remove delegate

--- packages/forge/src/lib/breadcrumb/index.ts
+++ packages/forge/src/lib/breadcrumb/index.ts
@@ -7,7 +7,6 @@ export * from './breadcrumb/breadcrumb.js';
 export * from './breadcrumb/breadcrumb-constants.js';
 export * from './crumb/crumb.js';
 export * from './crumb/crumb-constants.js';
-export * from './breadcrumb/breadcrumb-component-delegate.js';
 
 /** @deprecated Definition functions are deprecated and replaced with side effect imports (`import '@tylertech/forge/breadcrumb'`). */
 export function defineBreadcrumbComponent(): void {
